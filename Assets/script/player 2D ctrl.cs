13fa2a2863cbb08ba29dab06e302a88f9ed7e448
Alijah00 <163976527+Alijah00@users.noreply.github.com>
2026-04-23 10:21:17 +0700

    script

---
+++ Assets/script/player 2D ctrl.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player2Dctrl : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpForce = 450;
+    private Rigidbody2D rb;
+    private float moveInputValue;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current != null)
+        {
+            moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0);
+        }
+        rb.linearVelocity = new Vector2(moveInputValue * speed, rb.linearVelocity.y);
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
+        }
+    }
+}
