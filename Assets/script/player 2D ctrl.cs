ec598d6a851088d30f017ce892fb83af96897837
Alijah00 <163976527+Alijah00@users.noreply.github.com>
2026-04-23 10:49:11 +0700

    phy mat

--- Assets/script/player 2D ctrl.cs
+++ Assets/script/player 2D ctrl.cs
@@ -7,6 +7,7 @@ public class player2Dctrl : MonoBehaviour
     public float jumpForce = 450;
     private Rigidbody2D rb;
     private float moveInputValue;
+    private bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,9 +22,21 @@ public class player2Dctrl : MonoBehaviour
             moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0);
         }
         rb.linearVelocity = new Vector2(moveInputValue * speed, rb.linearVelocity.y);
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
         {
             rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
